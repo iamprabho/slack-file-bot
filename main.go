90fdd0b1f900b5bd838bfef5ab60d573d6a7a58a
Prathamesh <prathameshbhor000@gmail.com>
2023-02-25 18:19:01 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN","xoxb-4859211990387-4844809092999-kBD4CFE7tKuwnctKpI232uMV")
+
+	os.Setenv("CHANNEL_ID","C04QUPK7GMD")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))	
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"Bonafide.pdf"}
+	for i:= 0;i<len(fileArr);i++{
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File: fileArr[i],
+		}
+		file,err := api.UploadFile(params)
+		if err!= nil{
+			fmt.Printf("%s\n",err)
+			return
+		}
+		fmt.Printf("Name: %s , URL:%s\n",file.Name,file.URL)
+
+	}
+
+
+}
